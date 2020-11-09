7364b9bb7271387674c97e8ad558a16c7d9e1ece
Emilsone <lawsonezekiel68@gmail.com>
2020-11-09 16:17:55 +0100

    refactor footer code

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,16 +4,54 @@
       <div class="w-full flex flex-col md:flex-row py-6">
         <div class="flex-1 mb-6">
           <logo />
+          <p class="text-sm">
+            Welcome to Organik. Our products are freshly harvested, washed ready
+            for box and finally delivered from our family farm right to your
+            doorstep.
+          </p>
         </div>
 
         <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Company</p>
+          <p class="uppercase text-gray-500 md:mb-6">INFORMATION</p>
           <ul class="list-reset mb-6">
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
                 class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Official Blog</a
+                >New Products</a
+              >
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >Top Sellers</a
+              >
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >Our Blog</a
+              >
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >About Our Shop</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div class="flex-1">
+          <p class="uppercase text-gray-500 md:mb-6">USEFUL LINKS</p>
+          <ul class="list-reset mb-6">
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >FAQ</a
               >
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
@@ -27,25 +65,6 @@
               <a
                 href="#"
                 class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Contact</a
-              >
-            </li>
-          </ul>
-        </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Links</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >FAQ</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
                 >Help</a
               >
             </li>
@@ -53,7 +72,7 @@
               <a
                 href="#"
                 class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Support</a
+                >Secure Shopping</a
               >
             </li>
           </ul>
@@ -78,33 +97,18 @@
           </ul>
         </div>
 
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Social</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Facebook</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Linkedin</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Twitter</a
-              >
-            </li>
-          </ul>
+          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+            <div class="flex xl:flex-no-wrap md:flex-no-wrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
+              <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                <label for="footer-field" class="leading-7 text-sm text-gray-600">Enter your email address for our mailing list to keep yourself updated.</label>
+                <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              </div>
+              <button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+            </div>
+            
         </div>
-      </div>
+    </div>
     </div>
     <div class="bg-gray-200">
       <footerpayment />
@@ -118,7 +122,7 @@ import FooterPayment from '@/components/FooterPayment'
 export default {
   name: 'TheFooter',
   components: {
-     Logo,
+    Logo,
     FooterPayment
   }
 }
