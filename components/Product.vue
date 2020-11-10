da56347dfa7c15bdcfa3ade4b794289bff052bed
Emilsone <lawsonezekiel68@gmail.com>
2020-11-10 17:11:35 +0100

    add delivery process

--- components/Product.vue
+++ components/Product.vue
@@ -1,136 +1,92 @@
 <template>
   <section class="text-gray-700 body-font">
     <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-        <div class="p-4 md:w-1/3 md:mb-0 mb-6 flex">
-          <div
-            class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"
-          >
+      <div class="flex flex-col text-center w-full mb-20">
+        <h1
+          class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+        >
+          HOW WE WORK
+        </h1>
+      </div>
+      <div class="flex flex-wrap -m-4 text-center">
+        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
             <svg
               fill="none"
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-6 h-6"
+              class="text-indigo-500 w-12 h-12 mb-3 inline-block"
               viewBox="0 0 24 24"
             >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              <path d="M8 17l4 4 4-4m-4-5v9"></path>
+              <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
             </svg>
-          </div>
-          <div class="flex-grow pl-6">
-            <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
-              Shooting Stars
+            <h2 class="title-font font-medium text-3xl text-gray-900">
+              24/7 CUSTOMER SERVICE
             </h2>
-            <p class="leading-relaxed text-base">
-              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
-              mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge
-              tilde.
-            </p>
-            <a class="mt-3 text-indigo-500 inline-flex items-center"
-              >Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
+            <p class="leading-relaxed">Call us any time</p>
           </div>
         </div>
-        <div class="p-4 md:w-1/3 md:mb-0 mb-6 flex">
-          <div
-            class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"
-          >
+        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
             <svg
               fill="none"
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-6 h-6"
+              class="text-indigo-500 w-12 h-12 mb-3 inline-block"
               viewBox="0 0 24 24"
             >
-              <circle cx="6" cy="6" r="3"></circle>
-              <circle cx="6" cy="18" r="3"></circle>
+              <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+              <circle cx="9" cy="7" r="4"></circle>
+              <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+            </svg>
+            <h2 class="title-font font-medium text-3xl text-gray-900">
+              EASY ONLINE RETURNS
+            </h2>
+            <p class="leading-relaxed">Send Within 30 Days</p>
+          </div>
+        </div>
+        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="text-indigo-500 w-12 h-12 mb-3 inline-block"
+              viewBox="0 0 24 24"
+            >
+              <path d="M3 18v-6a9 9 0 0118 0v6"></path>
               <path
-                d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"
+                d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"
               ></path>
             </svg>
-          </div>
-          <div class="flex-grow pl-6">
-            <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
-              The Catalyzer
+            <h2 class="title-font font-medium text-3xl text-gray-900">
+              FREE SHIPPING GLOBALLY
             </h2>
-            <p class="leading-relaxed text-base">
-              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
-              mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge
-              tilde.
-            </p>
-            <a class="mt-3 text-indigo-500 inline-flex items-center"
-              >Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
+            <p class="leading-relaxed">Delivery in 4 Days</p>
           </div>
         </div>
-        <div class="p-4 md:w-1/3 md:mb-0 mb-6 flex">
-          <div
-            class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"
-          >
+        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
             <svg
               fill="none"
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-6 h-6"
+              class="text-indigo-500 w-12 h-12 mb-3 inline-block"
               viewBox="0 0 24 24"
             >
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
+              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
             </svg>
-          </div>
-          <div class="flex-grow pl-6">
-            <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
-              Neptune
-            </h2>
-            <p class="leading-relaxed text-base">
-              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
-              mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge
-              tilde.
-            </p>
-            <a class="mt-3 text-indigo-500 inline-flex items-center"
-              >Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
+            <h2 class="title-font font-medium text-3xl text-gray-900">REFUND PROCESSED</h2>
+            <p class="leading-relaxed">Return your money immediately</p>
           </div>
         </div>
       </div>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -77,38 +77,36 @@
             </li>
           </ul>
         </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Legal</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Terms</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Privacy</a
-              >
-            </li>
-          </ul>
-        </div>
 
-          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
-            <div class="flex xl:flex-no-wrap md:flex-no-wrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-              <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-                <label for="footer-field" class="leading-7 text-sm text-gray-600">Enter your email address for our mailing list to keep yourself updated.</label>
-                <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-              </div>
-              <button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+        <div class="flex-1 md:w-1/2 w-full px-4">
+          <h2
+            class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3"
+          >
+            SUBSCRIBE
+          </h2>
+          <div
+            class="flex xl:flex-no-wrap md:flex-no-wrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start"
+          >
+            <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+              <label for="footer-field" class="leading-7 text-sm text-gray-600"
+                >Enter your email address for our mailing list to keep yourself
+                updated.</label
+              >
+              <input
+                type="text"
+                id="footer-field"
+                name="footer-field"
+                class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
             </div>
-            
+            <button
+              class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
+            >
+              Button
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
     <div class="bg-gray-200">
       <footerpayment />
