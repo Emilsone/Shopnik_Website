54b0d705bbe6b4188e60f2b5a780ac8662fd71cf
Emilsone <lawsonezekiel68@gmail.com>
2020-11-24 09:32:56 +0100

    made footer responsive, add images to furniture page, change footer logo, change hero image

--- components/Hero.vue
+++ components/Hero.vue
@@ -6,7 +6,7 @@
       >
         <!--Left Col-->
         <div
-          class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
+          class="flex flex-col w-full md:w-2/5 justify-center lg:items-start items-center text-center md:text-left"
         >
           <p class="uppercase w-full">Looking for Good furnitures?</p>
           <h1 class="my-4 text-5xl font-bold leading-tight">
@@ -22,7 +22,7 @@
         </div>
         <!--Right Col-->
         <div class="w-full md:w-3/5 py-6 text-center">
-          <img class="w-full md:w-4/5 z-50" src="~/assets/img/hero.png" alt />
+          <img class="w-full md:w-4/5 z-50" src="~/assets/img/hero-img.png" alt />
         </div>
       </div>
     </div>
---
+++ components/LogoFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1
+    class="no-underline hover:no-underline font-bold text-2xl lg:text-4xl text-white px-4 py-4"
+  >Shopnik<span class="text-2xl">.</span>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'LogoFooter',
+}
+</script>
